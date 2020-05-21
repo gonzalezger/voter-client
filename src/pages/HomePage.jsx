2d82cc4dc761c58099fdc1ab355ffda9d2fbb14b
German Gonzalez <gercho.gonzalez@gmail.com>
2020-05-20 21:20:31 -0300

    use env variable to connect server

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import RoomPage from './RoomPage';
 
-const socket = io('http://127.0.0.1:3000');
+const socket = io(process.env.REACT_APP_SERVER_URL);
 
 export default function HomePage() {
   const [username, setUsername] = useState('');
